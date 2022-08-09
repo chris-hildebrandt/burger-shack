baf87b6a8c5752c2496aad14baf383afce8cf6e2
chris-hildebrandt <ch.hild15@gmail.com>
2022-08-08 22:41:43 -0600

    service up

--- server/controllers/BurgersController.js
+++ server/controllers/BurgersController.js
@@ -1,3 +1,35 @@
 
 import { BaseController } from "../utils/BaseController.js"
-export class BurgersController extends BaseController
+export class BurgersController extends BaseController {
+
+  constructor(){
+    super('/api/burgers')
+
+    this.router
+     .get('', this.getBurgers)
+     .get('/:burgerId', this.getBurgerById)
+     .post('', this.createBurger)
+     .put('/:burgerId', this.editBurger)
+     .delete('/:burgerId', this.deleteBurger)
+  }
+
+  async getBurgers(req, res, next){
+    try {
+      let res = await burgersService.getBurgers()
+      res.send(burgers)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getBurgerById(req, res, next){
+    try {
+      let res = await this.getBurgerById(req.params.burgerId)
+      res.send(burger)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+
+}
