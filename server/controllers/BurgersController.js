e78dbf03cd15b9ba1edf0c9e6a82c522a4a9e943
chris-hildebrandt <ch.hild15@gmail.com>
2022-08-09 10:16:56 -0600

    burger controller completed

--- server/controllers/BurgersController.js
+++ server/controllers/BurgersController.js
@@ -1,19 +1,20 @@
 
+import { burgersService } from "../services/BurgersService.js"
 import { BaseController } from "../utils/BaseController.js"
 export class BurgersController extends BaseController {
 
-  constructor(){
+  constructor() {
     super('/api/burgers')
 
     this.router
-     .get('', this.getBurgers)
-     .get('/:burgerId', this.getBurgerById)
-     .post('', this.createBurger)
-     .put('/:burgerId', this.editBurger)
-     .delete('/:burgerId', this.deleteBurger)
+      .get('', this.getBurgers)
+      .get('/:burgerId', this.getBurgerById)
+      .post('', this.createBurger)
+      .put('/:burgerId', this.editBurger)
+      .delete('/:burgerId', this.deleteBurger)
   }
 
-  async getBurgers(req, res, next){
+  async getBurgers(req, res, next) {
     try {
       let res = await burgersService.getBurgers()
       res.send(burgers)
@@ -22,7 +23,7 @@ export class BurgersController extends BaseController {
     }
   }
 
-  async getBurgerById(req, res, next){
+  async getBurgerById(req, res, next) {
     try {
       let res = await this.getBurgerById(req.params.burgerId)
       res.send(burger)
@@ -31,5 +32,32 @@ export class BurgersController extends BaseController {
     }
   }
 
+  async createBurger(req, res, next) {
+    try {
+      let burgerData = req.body
+      let burger = await burgersService.createBurger(burgerData)
+      res.send(burger)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async editBurger(req, res, next) {
+    try {
+      let burger = await burgersService.editBurger(req.params.burgerId, req.body)
+      res.send(burger)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async deleteBurger(req, res, next) {
+    try {
+      let burger = await burgersService.deleteBurger(req.params.burgerId)
+      res.send(burger)
+    } catch (error) {
+      next(error)
+    }
+  }
 
 }
