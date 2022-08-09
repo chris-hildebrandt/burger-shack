39d3ccc10c6a9553a710f743e4a3b832c2012a65
chris-hildebrandt <ch.hild15@gmail.com>
2022-08-08 22:29:24 -0600

    lab

--- server/controllers/ValuesController.js
+++ server/controllers/ValuesController.js
@@ -1,10 +1,21 @@
 import { valuesService } from '../services/ValueService'
 import BaseController from '../utils/BaseController'
 
+// NOTE you have to respin your server everytime you make a change
+// in postman you need the full address with the api and "door" address, and then you can make put,post,delete, etc. reqs. in the body tab you can send raw JSON strings which will just be objects { with "atr": values} puts need th id on the end of the address or they won't work
+// the FAKE_DB is stored as a const in the service scratch that, make a js file in the db folder
+// the cannot edit this file error is a debugging file, close it and edit the real one
+// reusing the getcatbyid function saves space, and lets us id check with the one error catch
+
+
 export class ValuesController extends BaseController {
   constructor() {
+    // the super is the door label
+    // when you have a class that extends  another class you must have a super in the ctor
     super('api/values')
+    // the router comes from the base controller
     this.router
+    // method chaining
       .get('', this.getAll)
       .post('', this.create)
       .delete('/:valueId', this.remove)
@@ -17,10 +28,12 @@ export class ValuesController extends BaseController {
    * @param {import("express").NextFunction} next
    */
   async getAll(req, res, next) {
+    // order of these ^ is important! don't remove one or change the order
     try {
       const values = await valuesService.find()
       return res.send(values)
     } catch (error) {
+      // logger.error() but this goes elsewhere because of next
       next(error)
     }
   }
